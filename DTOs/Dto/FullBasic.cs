2b4cd8e080240228db083915f892a680beff8dd3
kunah <57344355+kunah@users.noreply.github.com>
2025-03-11 12:44:40 +0100

    Version

--- DTOs/Dto/FullBasic.cs
+++ DTOs/Dto/FullBasic.cs
@@ -4,7 +4,7 @@ namespace DTOs.Dto
 {
     public partial class FullBasic
     {
-        public Int32 Test { get; set; } = -2040035567;
+        public Int32 Test { get; set; } = 722944099;
         public String DtoName { get; set; } = "";
         public String? Always { get; set; } = "";
         public SecondDto? model { get; set; }
