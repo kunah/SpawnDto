655d9b3a4bd1c216bb5ba182a6b48fe96cf77bc8
kunah <57344355+kunah@users.noreply.github.com>
2025-03-11 01:59:38 +0100

    rename

--- DTOs/Dto/FullBasic.cs
+++ DTOs/Dto/FullBasic.cs
@@ -4,7 +4,7 @@ namespace DTOs.Dto
 {
     public partial class FullBasic
     {
-        public Int32 Test { get; set; } = -731592327;
+        public Int32 Test { get; set; } = -1393650369;
         public String DtoName { get; set; } = "";
     }
 }
